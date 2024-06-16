532b0f1ad7b05f4d1dc4ea21d7fcb77cb182df53
Victor S. <victor@ton.org>
2024-06-16 16:29:27 +0300

    API method for dashboard

--- api.py
+++ api.py
@@ -31,8 +31,91 @@ time_intervals = [
 ]
 
 
+@app.route("/interval/<path:path>")
+def interval(path):
+    """Get averages on period of now-1h to now-60sec. 1h interval can
+    be changed to any value by setting `seconds` parameter."""
+
+    if path.find("/") != -1:
+        logger.debug(f"Invalid db path: {path}")
+        return {"error": "invalid path: / not allowed"}
+
+    if not Path(f"db/{path}.db").is_file():
+        logger.debug(f"Invalid db path: {path}")
+        return {"error": "no such db"}
+
+    interval_sec = int(request.args.get("seconds", 3600))
+
+    # to close connection in case of exception
+    class Connection:
+        def close(self):
+            pass
+
+    connection = Connection()
+
+    try:
+        connection = sqlite3.connect(f"db/{path}.db")
+        cursor = connection.cursor()
+
+        now = int(time.time())
+
+        cursor.execute(
+            f"""
+            SELECT COUNT(*), AVG(is_found),
+
+                   AVG(executed_in), MIN(executed_in),
+                   MAX(executed_in),
+                   -- variance of executed_in
+                   AVG(executed_in*executed_in) - AVG(executed_in)*AVG(executed_in),
+
+                   AVG(found_in), MIN(found_in),
+                   MAX(found_in),
+                   -- variance of found_in
+                   AVG(found_in*found_in) - AVG(found_in)*AVG(found_in)
+
+            FROM txs WHERE utime >= ?
+                AND utime <= ?
+            ORDER BY utime DESC LIMIT 1""",
+            (now - interval_sec, now - 60),
+        )
+
+        res = cursor.fetchone()
+
+        if res[0] == 0:
+            return {"error": "no txs in this period"}
+
+        result = {
+            "txs": res[0],
+            "success_rate": round(res[1] or 0, 4),
+            "executed_in_avg": round(res[2] or 0, 2),
+            "executed_in_min": res[3],
+            "executed_in_max": res[4],
+            # stdev = sqrt(variance)
+            "executed_in_sdev": round(math.sqrt(res[5] or 0), 2),
+            "found_in_avg": round(res[6] or 0, 2),
+            "found_in_min": res[7],
+            "found_in_max": res[8],
+            # stdev = sqrt(variance)
+            "found_in_sdev": round(math.sqrt(res[9] or 0), 2),
+        }
+
+        connection.close()
+        logger.debug(f"Completed request on /{path}")
+        return result
+
+    except Exception as e:
+        try:
+            connection.close()
+            logger.info("Forcefully, but successfully closed the connection")
+        except:
+            logger.warning("Failed to close connection")
+            pass
+        logger.error(str(e))
+        return {"error": str(e)}
+
+
 @app.route("/<path:path>")
-def stats(path):
+def get_processed(path):
     if path not in ["liteserver", "toncenter", "tonapi"]:
         logger.debug(f"Invalid endpoint: {path}")
         return {"error": "endpoint must be: liteserver, toncenter, tonapi"}
@@ -53,22 +136,6 @@ def stats(path):
         now = int(time.time())
 
         result = {}
-
-        # ex = {
-        #         "10m": {
-        #             "txs": 10,
-        #             "success_rate": 0.9,
-        #             "executed_in_avg": 4,
-        #             "executed_in_min": 1,
-        #             "executed_in_max": 8,
-        #             "executed_in_sdev": 5.3,
-        #             "found_in_avg": 20,
-        #             "found_in_min": 10,
-        #             "found_in_max": 40,
-        #             "found_in_sdev": 10.7,
-        #         }
-        #         "30m": { ... },
-        # }
 
         last_len = 0
         for interval_txt, interval_sec in time_intervals:
@@ -127,7 +194,6 @@ def stats(path):
         except:
             logger.warning("Failed to close connection")
             pass
-        raise e
         logger.error(str(e))
         return {"error": str(e)}
 
